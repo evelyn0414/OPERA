7cd7ddeaf7546dcf5552804712a5dbd9c5b8755a
XTxiatong <xiatongxt028@gmail.com>
2024-06-11 09:56:27 +0100

    updated by tx

--- scripts/multiple_pretrain.sh
+++ scripts/multiple_pretrain.sh
@@ -9,3 +9,16 @@ python -u src/pretrain/cola_training.py --data multiple\
         --encoder efficientnet\
         --title operaCE-test\
         --epoches 150
+
+
+python -u src/pretrain/mae_training.py --data multiple\
+        --covidbreath True\
+        --covidcough True\
+        --icbhicycle True\
+        --coughvid True\
+        --hf_lung True\
+        --covidUKexhalation True\
+        --covidUKcough True\
+        --encoder vit\
+        --title operaGT-test\
+        --epoches 150
