526b19bb9be4afb539dab26e90e003fd7cc37f82
XTxiatong <xiatongxt028@gmail.com>
2024-06-22 21:36:20 +0100

    Update benchmark.sh

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,12 +1,12 @@
 
 ## runing baselines
-sh scripts/eval_all.sh opensmile  >> result_opensmile.log
-sh scripts/eval_all.sh vggish  >> result_vggish.log
-sh scripts/eval_all.sh audiomae  >> result_audiomae.log
-sh scripts/eval_all.sh clap  >> result_clap.log
+sh scripts/eval_all.sh opensmile  >> cks/logs/result_opensmile.log
+sh scripts/eval_all.sh vggish  >> cks/logs/result_vggish.log
+sh scripts/eval_all.sh audiomae  >> cks/logs/result_audiomae.log
+sh scripts/eval_all.sh clap  >> cks/logs/result_clap.log
 
 
 ## runing opera-X
-sh scripts/eval_all.sh operaCT 768  >> result_operaCT.log
-sh scripts/eval_all.sh operaCE 1280  >> result_operaCE.log
-sh scripts/eval_all.sh operaGT 384 >> result_operaGT.log
+sh scripts/eval_all.sh operaCT 768  >> cks/logs/result_operaCT.log
+sh scripts/eval_all.sh operaCE 1280  >> cks/logs/result_operaCE.log
+sh scripts/eval_all.sh operaGT 384 >> cks/logs/result_operaGT.log
