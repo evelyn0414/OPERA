caf3d9faf898c1b72bae4e59ab93720b42aee29b
XTxiatong <xiatongxt028@gmail.com>
2024-06-22 21:52:05 +0100

    Update kauh_eval.sh

--- scripts/kauh_eval.sh
+++ scripts/kauh_eval.sh
@@ -1,3 +1,10 @@
+list="opensmile vggish audiomae clap"  
+for i in $list;  
+do  
+python src/benchmark/processing/kauh_processing.py --pretrain $i 
+python src/benchmark/linear_eval.py --task kauh --pretrain $i
+done 
+
 model_name="operaCT"
 dim=768
 python src/benchmark/processing/kauh_processing.py --pretrain $model_name --dim $dim
@@ -13,9 +20,4 @@ dim=384
 python src/benchmark/processing/kauh_processing.py --pretrain $model_name --dim $dim
 python src/benchmark/linear_eval.py --task kauh --pretrain $model_name --dim $dim
 
-list="opensmile vggish clap audiomae"  
-for i in $list;  
-do  
-python src/benchmark/processing/kauh_processing.py --pretrain $i 
-python src/benchmark/linear_eval.py --task kauh --pretrain $i
-done 
+
