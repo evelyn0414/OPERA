622dddaca85d6e6456234ab669b2fca96e0fd598
XTxiatong <xiatongxt028@gmail.com>
2024-06-22 21:35:34 +0100

    updated

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,8 +2,9 @@
 ## runing baselines
 sh scripts/eval_all.sh opensmile  >> result_opensmile.log
 sh scripts/eval_all.sh vggish  >> result_vggish.log
-sh scripts/eval_all.sh clap  >> result_clap.log
 sh scripts/eval_all.sh audiomae  >> result_audiomae.log
+sh scripts/eval_all.sh clap  >> result_clap.log
+
 
 ## runing opera-X
 sh scripts/eval_all.sh operaCT 768  >> result_operaCT.log
